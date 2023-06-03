5af74c81beea13819d2f7a00c3046c110bc1e1f0
aruzaev <ruzaev.artem21@gmail.com>
2023-06-03 18:38:46 -0400

    day 1 added

---
+++ day1.py
@@ -0,0 +1,6 @@
+
+def bandNameGenerator(cityName, petName):
+    print("Welcome to the Band Name Generator")
+    print("Your band name could be: " + cityName + " " + petName)
+
+bandNameGenerator(input("What is the name of the city you grew up in?\n"), input("What is the name of your pet?\n"))
