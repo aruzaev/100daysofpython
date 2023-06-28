8708ad2d239e8a8be680b2c0de84da950965a9ed
aruzaev <ruzaev.artem21@gmail.com>
2023-06-28 13:09:03 -0400

    added day3

--- day3.py
+++ day3.py
@@ -19,7 +19,7 @@ if height >= 120:
 else:
     print("You aren't tall enough to ride this ride") """
 
-height = float(input("Enter your height: "))
+""" height = float(input("Enter your height: "))
 weight = float(input("Enter your weight: "))
 
 bmi = round(float(weight / height**2))
@@ -33,4 +33,87 @@ elif bmi < 30:
 elif bmi < 35:
     print(f"Your BMI is {bmi}, you are obese.")
 else:
-    print(f"Your BMI is {bmi}, you are clinically obese.")
+    print(f"Your BMI is {bmi}, you are clinically obese.") """
+""" 
+year = int(input("Which year do you want to check? "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.") """
+""" 
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+wantsPepperoni = input("Do you want pepperoni? Y or N ")
+extraCheese = input("Do you want extra cheese? Y or N ")
+
+bill = 0
+
+if size == "S":
+    bill = 15
+    if wantsPepperoni == "Y":
+        bill += 2
+    if extraCheese == "Y":
+        bill +=1
+elif size == "M":
+    bill = 20
+    if wantsPepperoni == "Y":
+        bill += 3
+    if extraCheese == "Y":
+        bill +=1
+elif size == "L":
+    bill = 25
+    if wantsPepperoni == "Y":
+        bill += 3
+    if extraCheese == "Y":
+        bill +=1
+
+    
+
+print(f"Your final bill is: ${bill}") """
+
+""" print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is your their name? \n")
+
+name1Lower = name1.lower()
+name2Lower = name2.lower()
+
+totalString = name1Lower + name2Lower
+
+trueNumber = totalString.count("t") + totalString.count("r") + totalString.count("u") + totalString.count("e")
+loveNumber = totalString.count("l") + totalString.count("o") + totalString.count("v") + totalString.count("e")
+
+score = str(trueNumber) + str(loveNumber)
+score = int(score)
+
+if score < 10 or score > 90:
+    print(f"Your score is {score}, you go together like coke and mentos.")
+elif score >= 40 and score <= 50:
+    print(f"Your score is {score}, you are alright together.")
+else:
+    print(f"Your score is {score}.")
+ """
+
+print("Welcome to Treasure Island. \nYour mission is to find the treasure.")
+
+leftRightPrompt = input("You're at a crossroad. Where do you want to go? Type \"left\" or \"right\". ").lower()
+
+if leftRightPrompt == "right":
+    swimOrWaitPrompt = input("You've come to a lake. There is an island in the middle of the lake. "
+                             "Type \"wait\" to wait for a boat. Type \"swim\" to swim across. ").lower()
+    if swimOrWaitPrompt == "wait":
+        whichDoor = input("You arrive at the island unharmed. There is a house with 3 doors. "
+                          "One red, one yellow and one blue. Which colour do you choose? ").lower()
+        if whichDoor == "yellow":   
+            print("You found the treasure! You win!")
+        else:
+            print("you died game over")
+    else:
+        print("You drowned, game over!")
+else:
+    print("You fucking died, game over bitch")
