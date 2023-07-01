0be38c761e36d83081c0c290f0174a0db706671d
aruzaev <ruzaev.artem21@gmail.com>
2023-07-01 15:43:30 -0400

    added day4

--- day4.py
+++ day4.py
@@ -21,23 +21,58 @@ dirtyDozen = [fruits, vegetables]
 
 print(dirtyDozen) """
 
-# 🚨 Don't change the code below 👇
-row1 = ["⬜️","️⬜️","️⬜️"]
-row2 = ["⬜️","⬜️","️⬜️"]
-row3 = ["⬜️️","⬜️️","⬜️️"]
-map = [row1, row2, row3]
-print(f"{row1}\n{row2}\n{row3}")
-position = input("Where do you want to put the treasure? ")
-# 🚨 Don't change the code above 👆
+# row1 = ["⬜️", "⬜️", "⬜️"]
+# row2 = ["⬜️", "⬜️", "⬜️"]
+# row3 = ["⬜️", "⬜️", "⬜️"]
+# map = [row1, row2, row3]
+# print(f"{row1}\n{row2}\n{row3}")
 
-#Write your code below this row 👇
+# position = input("Where do you want to put the treasure? ")
 
-coords = position.split()
-print(coords)
+# r = int(position[0]) - 1
+# c = int(position[1]) - 1
 
+# map[r][c] = "x"
 
-#Write your code above this row 👆
+# print(f"{row1}\n{row2}\n{row3}")
 
-# 🚨 Don't change the code below 👇
-print(f"{row1}\n{row2}\n{row3}")
+import random
 
+moves = ['''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+''', '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+''', '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+''']
+outcomes = ["win", "lose", "tie"]
+
+# print(moves[2] + "\nYou " + outcomes[0])
+
+playerChoice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors\n"))
+pcChoice = random.randint(0, 2)
+
+if pcChoice == playerChoice:
+    print(moves[playerChoice] + "\nComputer chose:" + moves[pcChoice] + "\nYou " + outcomes[2])
+
+elif (pcChoice == 0 and playerChoice == 2) or (pcChoice == 2 and playerChoice == 1) or (pcChoice == 1 and playerChoice == 0):
+    print(moves[playerChoice] + "\nComputer chose:" + moves[pcChoice] + "\nYou " + outcomes[1])
+
+elif (playerChoice == 0 and pcChoice == 2) or (playerChoice == 2 and pcChoice == 1) or (playerChoice == 1 and pcChoice == 0):
+    print(moves[playerChoice] + "\nComputer chose:" + moves[pcChoice] + "\nYou " + outcomes[0])
+    
