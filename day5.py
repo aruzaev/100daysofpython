8c5a1bd29ba2b7202657e7163d94f304e4484eaa
aruzaev <ruzaev.artem21@gmail.com>
2023-07-11 14:11:30 -0400

    added day5

---
+++ day5.py
@@ -0,0 +1,52 @@
+# fruits = ["Apple", "Pear", "Orange"]
+
+# for i in range(0, len(fruits)):
+#     print(str(i + 1) + ". " + fruits[i])
+
+# student_heights = input("Input a list of student heights ").split()
+# for n in range(0, len(student_heights)):
+#     student_heights[n] = int(student_heights[n])
+
+# numberOfStudents = 0 
+# totalHeight = 0
+
+# for student in student_heights:
+#     numberOfStudents += 1
+
+# for height in student_heights:
+#     totalHeight += height
+
+# print(round(totalHeight / numberOfStudents))
+
+# studentScores = input("Input a list of student scores ").split()
+
+# for n in range(0, len(studentScores)):
+#     studentScores[n] = int(studentScores[n])
+
+# print(studentScores)
+
+# highestScore = 0
+
+# for i in studentScores:
+#     if i > highestScore:
+#         highestScore = i
+
+# print(f"The highest score in the class is: {highestScore}")
+
+# sum = 0
+
+# for number in range(2, 101, 2):
+#     sum += number
+
+# print(sum)
+
+# for number in range(1, 101):
+#     if number % 5 == 0 and number % 3 == 0:
+#         print("FizzBuzz")
+#     elif number % 5 == 0:
+#         print("Buzz")
+#     elif number % 3 == 0:
+#         print("Fizz")
+#     else:
+#         print(number)
+
